ff9cdff89b360ce13f7d020b866325002311917a
Matei Pelletier <pelletiermatei@gmail.com>
2023-02-27 13:15:55 -0500

    removed redundances

--- Synonymes_Filter.py
+++ Synonymes_Filter.py
@@ -1,49 +1,42 @@
 import numpy as np
 
 class Synonymes_Filter:
-    def __init__(self, word_to_search, nb_results, score_strategy, word_indices):
+    def __init__(self, word_to_search, nb_results, score_strategy, cooc_matrix, word_indices):
         self.word_to_search = word_to_search
         self.nb_results = nb_results
         self.score_strategy = score_strategy
         self.stop_words = {"le", "ta", "ton", "la", "de", "et", "l", "à"}
         self.word_indices = word_indices
+        self.cooc_matrix = cooc_matrix
 
-    def scalar_product(self, cooc_matrix):
+    def get_score(self):
         row_index = self.get_index_from_word()
+        target_row = self.cooc_matrix[row_index]
         score = []
-        target_row = cooc_matrix[row_index]
-        for row in cooc_matrix:
-            score.append(np.dot(row, target_row))
-        self.score = score
-
-    def city_block(self, cooc_matrix):
-        row_index = self.get_index_from_word()
-        target_row = cooc_matrix[row_index]
-        score = []
-        for row in cooc_matrix:
-            score.append(np.abs(row - target_row).sum())
-        self.score = score
-
-    def least_square(self, cooc_matrix):
-        row_index = self.get_index_from_word()
-        target_row = cooc_matrix[row_index]
-        score = []
-        for row in cooc_matrix:
-            score.append(np.sum(np.square(row - target_row)))
+        for row in self.cooc_matrix:
+            if self.score_strategy == 1:
+                score.append(np.sum(np.square(row - target_row)))
+            elif self.score_strategy == 2:
+                score.append(np.abs(row - target_row).sum())
+            elif self.score_strategy == 3:
+                score.append(np.dot(row, target_row))
         self.score = score
 
     def get_index_from_word(self):
         return self.word_indices[self.word_to_search]
 
-    def get_top_words(self, top_target):
-        index_sort = np.argsort(self.score)[::-1]
+    def get_top_words(self):
+        if self.score_strategy > 1:
+            index_sort = np.argsort(self.score)[::-1]
+        else:
+            index_sort = np.argsort(self.score)
         flipped_word_indices = {v: k for k, v in self.word_indices.items()}
         top_counter = 0
         index_counter = 0
 
         stop_words = open('stop_words_french.txt', 'r', encoding="utf-8").read()
 
-        while top_counter <= top_target:
+        while top_counter <= self.nb_results:
             index = index_sort[index_counter]
 
             if flipped_word_indices[index] in stop_words:
--- main.py
+++ main.py
@@ -6,22 +6,16 @@ import Synonymes_Training as st
 def main():
     chemin = "DonQuichotteUTF8.txt"
     enc = "utf-8"
-    top_target = 5
+
 
     trainer = st.Synonymes_Training(chemin, enc)
     trainer.read()
     trainer.create_cooccurrence_matrix(5)
 
 
-    syn_filter = sf.Synonymes_Filter("sancho", 5, 1, trainer.word_indices)
-    syn_filter.scalar_product(trainer.cooc_matrix)
-    syn_filter.get_top_words(top_target)
-    print("--------------------------")
-    syn_filter.city_block(trainer.cooc_matrix)
-    syn_filter.get_top_words(top_target)
-    print("--------------------------")
-    syn_filter.least_square(trainer.cooc_matrix)
-    syn_filter.get_top_words(top_target)
+    syn_filter = sf.Synonymes_Filter("tranquille", 5, 1, trainer.cooc_matrix, trainer.word_indices)
+    syn_filter.get_score()
+    print(syn_filter.get_top_words())
 
     return 0
 
